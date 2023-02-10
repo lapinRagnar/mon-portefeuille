b7c321493bd51912169fe67800fc062d152c27ae
lapinRagnar <lapinragnar@gmail.com>
2023-02-10 10:33:13 +0100

    ajouter une sphere

--- src/components/MonPremierThreeJs.vue
+++ src/components/MonPremierThreeJs.vue
@@ -23,19 +23,29 @@
     const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000)
     const renderer = new THREE.WebGLRenderer({ antialias: true })
 
-    console.log('scene', scene)
-
     renderer.setSize(width, height)
     container.value.appendChild(renderer.domElement)
 
+    // pour ajouter le cube
     const geometry = new THREE.BoxGeometry(1, 1, 1)
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
     const cube = new THREE.Mesh(geometry, material)
     scene.add(cube)
 
+    // pour ajouter la sphere
+    const sphereGeometry = new THREE.SphereGeometry(2, 5, 5)
+    const sphereMaterial = new THREE.MeshBasicMaterial({
+      color: 0x0000FF,
+      wireframe: true
+    })
+    const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
+    scene.add(sphere)
+
+    // pour les axes
     const axesHelper = new THREE.AxesHelper(5)
     scene.add(axesHelper)
 
+    // l'orbite
     const orbit = new OrbitControls(camera, renderer.domElement)
 
     // camera.position.z = 5
